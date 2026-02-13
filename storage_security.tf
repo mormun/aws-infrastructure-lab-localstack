cdc80e88f1265db739aa16df7527c3e732913884
Juanjo <88036831+mormun@users.noreply.github.com>
2026-02-13 13:42:10 +0100

    Create storage_security.tf

---
+++ storage_security.tf
@@ -0,0 +1,18 @@
+resource "aws_security_group" "jenkins_sg" {
+  name   = "jenkins-sg"
+  vpc_id = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Permite acceso a Jenkins
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"] # Permite salida a internet
+  }
+}
